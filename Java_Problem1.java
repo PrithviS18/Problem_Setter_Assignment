db1befd7166ae446c72a5368797c5e3f8f7ca30d
Prithvi Sen <113365172+PrithviS18@users.noreply.github.com>
2024-06-15 19:01:01 +0530

    Update Java_Problem1.java

--- Java_Problem1.java
+++ Java_Problem1.java
@@ -5,7 +5,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = 1; 
-        int n = sc.nextInt();
         while (t-- > 0) {
             solve(sc);
         }
